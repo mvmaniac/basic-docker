3d515b18b8d9890fe8b0da29386df6fe81c62c3b
yhs <mvmaniaz@gmail.com>
2020-12-02 21:21:59 +0900

    Imporve: extract db environment

--- 04-react-nodejs-based/back-end/src/db.js
+++ 04-react-nodejs-based/back-end/src/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'mysql',
-  user: 'root',
-  password: '1234',
-  database: 'docker_db'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
